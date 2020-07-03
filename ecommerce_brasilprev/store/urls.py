a0621d223af3b12f8ae97a048e14164c2c9daf18
Zebu777 <adrianomsancho@gmail.com>
2020-07-03 01:18:08 -0300

    Inserted Product details and footer.

--- ecommerce_brasilprev/store/urls.py
+++ ecommerce_brasilprev/store/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.aboutPage, name='about'),
+    path('produto/', views.productPage, name='produto'),
 ]
