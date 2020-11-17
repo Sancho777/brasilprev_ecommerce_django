5cef34629933c48660774547ff5e4877889a83c2
Zebu777 <adrianomsancho@gmail.com>
2020-11-17 18:10:25 -0300

    heroku config files

---
+++ store/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('category/<slug:category_slug>', views.home, name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>', views.productPage, name='product_detail'),
+    path('cart/', views.cart, name='cart'),
+    path('newproduct/', views.newProductView, name='newproduct'),
+
+    path('account/create/', views.signupView, name='signup'),
+    path('account/signin/', views.signinView, name='signin'),
+    path('account/signout/', views.signoutView, name='signout'),
+]
---
+++ store/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Category, Product, Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import Group, User
+from .forms import SignUpForm, newProductForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+
+# Create your views here.
+
+def home(request, category_slug=None):
+    category_page = None
+    products = None
+    if category_slug!=None:
+        category_page = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category_page, available=True)
+    else:
+        products = Product.objects.all().filter(available=True)
+    return render(request, 'home.html', {'category': category_page, 'products': products})
+
+
+def productPage(request, category_slug, product_slug):
+    try:
+        product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    return render(request, 'product.html', {'product': product})
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def newProduct(request):
+    return render(request, 'newproduct.html')
+
+def signupView(request):
+    if request.method =='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            signup_user = User.objects.get(username=username)
+            customer_group = Group.objects.get(name='Costumer')
+            customer_group.user_set.add(signup_user)
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+def signinView(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                return redirect('signup')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'signin.html', {'form': form})
+
+def signoutView(request):
+    logout(request)
+    return redirect('signin')
+
+def newProductView(request):
+    form = newProductForm()
+
+    if request.method == 'POST':
+        form = newProductForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return home(request)
+        else:
+            print("ERROR FORM INVALID")
+
+
+    return render(request, 'newproduct.html', {'form': form})
