0c54612696e669f1852c4c50c8671021da39160d
Zebu777 <adrianomsancho@gmail.com>
2020-11-17 13:14:58 -0300

    Change labels ons signin form

--- ecommerce_brasilprev/store/forms.py
+++ ecommerce_brasilprev/store/forms.py
@@ -4,14 +4,14 @@ from django.contrib.auth.models import User
 from .models import Product
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=100, required=True)
-    last_name = forms.CharField(max_length=100, required=True)
+    primeiro_nome = forms.CharField(max_length=100, required=True)
+    sobrenome = forms.CharField(max_length=100, required=True)
     email = forms.EmailField(max_length=250, help_text='ex: youremail@mail.com', required=True)
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username', 'password1', 'password2', 'email')
-        labels = {'first_name':'Nome', 'last_name':'Sobrenome'}
+        fields = ('primeiro_nome', 'sobrenome', 'username', 'password1', 'password2', 'email')
+        labels = {'primeiro_nome':'Nome', 'sobrenome':'Sobrenome'}
 
 class newProductForm(forms.ModelForm):
 
